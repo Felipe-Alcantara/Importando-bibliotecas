58eec79e24919eb257f6c42728ae10d573c51a57
Felipe Alcântara <FelipeTheFeh@gmail.com>
2024-03-28 10:05:30 -0300

    Melhoria da Interface Gráfica com Tkinter
    O código foi aprimorado para incluir um título na janela (“Janelinha”) e um rótulo com texto (“Linha de texto”). Além disso, o rótulo agora tem um fundo cinza e é centralizado na janela. Essas mudanças melhoram a aparência da interface gráfica e tornam o programa mais informativo para o usuário.

--- Janela Simples com Tkinter.py
+++ Janela Simples com Tkinter.py
@@ -1,9 +1,15 @@
 from tkinter import *
 
 window = Tk()
-
-window = Tk()
-window.title("")
+window.title("Janelinha")
 window.geometry("500x500")
-equation_text = ""
+
+# Cria um Frame interno com fundo cinza
+inner_frame = Frame(window, bg='gray')
+inner_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+equation_text = "Linha de texto"
+label = Label(inner_frame, text=equation_text, font=("Helvetica", 30), fg='black', bg='gray')
+label.pack()
+
 window.mainloop()
